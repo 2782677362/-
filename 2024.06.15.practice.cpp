3dbbbc6cf75f39534a8c2fcf07fdc60d49605e19
2782677362 <147299781+2782677362@users.noreply.github.com>
2024-06-16 21:46:06 +0800

    字符串中的第一个唯一字符

--- 2024.06.15.practice.cpp
+++ 2024.06.15.practice.cpp
@@ -2,13 +2,13 @@
 #include <string>
 using namespace std;
 
-//����һ���ַ���s���ҵ����ĵ�һ�����ظ����ַ���������������������������ڣ��򷵻�-1��
-//sֻ����Сд��ĸ
-//ʾ�� 1������: s = "leetcode"  ��� : 0
-//ʾ�� 2 :����: s = "loveleetcode"  ��� : 2
-//ʾ�� 3 :����: s = "aabb"  ��� : -1
+//给定一个字符串s，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回-1。
+//s只包含小写字母
+//示例 1：输入: s = "leetcode"  输出 : 0
+//示例 2 :输入: s = "loveleetcode"  输出 : 2
+//示例 3 :输入: s = "aabb"  输出 : -1
 
-//�ⷨһ��������⣬ʱ�临�Ӷ�O(N*N)
+//解法一：暴力求解，时间复杂度O(N*N)
 class Solution1 {
 public:
     static int firstUniqChar(string s) 
@@ -34,7 +34,7 @@ public:
     }
 };
 
-//�ⷨ����ӳ�䣬ʱ�临�Ӷ�O(N)
+//解法二：映射，时间复杂度O(N)
 class Solution2 {
 public:
     static int firstUniqChar(string s)
